21d842de67423ca41ddd6ff5b5f99db40eca3dba
aidanwood98 <39964426+aidanwood98@users.noreply.github.com>
2018-08-09 16:10:07 -0400

    Add files via upload

--- DroneApp.Android/MainActivity.cs
+++ DroneApp.Android/MainActivity.cs
@@ -1,12 +1,16 @@
-﻿
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Xamarin;
 using Plugin.CurrentActivity;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using TK.CustomMap.Droid;
+
 namespace DroneApp.Droid
 {
-    [Activity(Label = "DroneApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Dronemates", Icon = "@drawable/DronematesLogo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         internal static MainActivity Instance { get; private set; }
@@ -22,7 +26,19 @@ namespace DroneApp.Droid
             global::Xamarin.Forms.Forms.Init(this, bundle);
             FormsGoogleMaps.Init(this, bundle); 
             FormsMaps.Init(this, bundle);
-            LoadApplication(new App ());
+            TKGoogleMaps.Init(this, bundle);
+            var width = Resources.DisplayMetrics.WidthPixels;
+            var height = Resources.DisplayMetrics.HeightPixels;
+            var density = Resources.DisplayMetrics.Density;
+
+            App.ScreenWidth = (width - 0.5f) / density;
+            App.ScreenHeight = (height - 0.5f) / density;
+
+            LoadApplication(new App());
+
+            AppCenter.Start("66fd97a2-5b59-45a3-afdf-1af54967204a",
+                   typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("66fd97a2-5b59-45a3-afdf-1af54967204a", typeof(Analytics), typeof(Crashes));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
         {
